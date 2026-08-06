e64fbd859134ee5b942849de3e337c63c307cc16
sontodosunoshijosdeputa2026-lgtm <sontodosunoshijosdeputa2026@gmail.com>
2026-08-06 15:05:19 -0300

    Create Conversation.js

---
+++ models/Conversation.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }],
+  relatedOffer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Offer'
+  },
+  lastMessage: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message'
+  },
+  lastMessageAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+conversationSchema.index({ participants: 1 });
+
+module.exports = mongoose.model('Conversation', conversationSchema);
