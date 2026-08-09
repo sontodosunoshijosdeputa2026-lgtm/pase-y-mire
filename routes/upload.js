eb441a227bc90556a215f23d23ffa64fd1d99691
sontodosunoshijosdeputa2026-lgtm <sontodosunoshijosdeputa2026@gmail.com>
2026-08-09 19:01:27 -0300

    Create upload.js

---
+++ routes/upload.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  return res.status(200).json({ 
+    success: true, 
+    message: 'Almacenamiento delegado a Supabase Storage' 
+  });
+});
+
+module.exports = router;
